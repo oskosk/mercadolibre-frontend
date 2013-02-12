cfbc1c8a1215267fbefa718df19e4c269e7a7c13
Christian Gastrell <cgastrell@gmail.com>
2013-02-12 02:57:45 -0300

    Agregue categoria.html y prettifie home. Update de index

--- index.php
+++ index.php
@@ -1,20 +1,39 @@
 <?php
 include_once('config.inc.php');
 include_once('Mercadolibre.class.php');
-
+error_reporting(E_ALL);
 $seller_id = $MERCADOLIBRE['seller_id'];
 if (isset($_GET['seller_id']) ) {
 	$seller_id=$_GET['seller_id'];
 }
+
+//es para pedir una categoria por default, aun cuando no se especifique ninguna
+//esto deberia pasar a config
+$categoria_base = "MLA1648";
+
+$categoria = null;
+$esCategoria = false;
+if(isset($_GET['categoria']))
+{
+	$categoria = $_GET['categoria'];
+	//en este caso tengo que listar los productos uno tras otro
+	//tiene que ser una template distinta: categoria.html
+	$esCategoria = true;
+}else{
+	$categoria = $categoria_base;
+	//en este caso se muestra el home que agrupa los productos
+	//por "sub" categorias
+	$esCategoria = false;
+}
+
 $seller_geocoding = $MERCADOLIBRE['seller_geocoding'];
 $seller_data = MercadoLibre::sellerData($seller_id);
 $payment_methods = MercadoLibre::sellerAcceptedPaymentMethods($seller_id);
-$seller_productos = MercadoLibre::productosBySeller($seller_id);
+$seller_productos = MercadoLibre::productosBySeller($seller_id,$categoria);
 $seller_destacados = MercadoLibre::productosDestacadosBySeller($seller_id);
 //die(print_r($seller_destacados,true));
 $seller_nickname =  $seller_data['nickname'];
-
 $titulo = $seller_nickname . ' - ' . $SITE_SUBTITLE;
-include_once('home.html');
+if(!$esCategoria) include_once('home.html'); else include_once('categoria.html');
 
 ?>
